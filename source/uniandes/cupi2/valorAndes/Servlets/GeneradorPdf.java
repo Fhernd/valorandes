d7c74a5e6a3a0c4779d4a03f0d5865fe0a1f3d97
John Learner Ortiz Ordoñez <johnortizo@outlook.com>
2014-09-24 13:43:00 -0500

    Embeber PDF de demostración.

--- source/uniandes/cupi2/valorAndes/Servlets/GeneradorPdf.java
+++ source/uniandes/cupi2/valorAndes/Servlets/GeneradorPdf.java
@@ -1,19 +1,26 @@
 package uniandes.cupi2.valorAndes.Servlets;
 
-import javax.servlet.http.*;  
-import javax.servlet.*;  
-import java.io.*;  
-public class GeneradorPdf extends HttpServlet{  
-public void doGet(HttpServletRequest req,HttpServletResponse res)  
-throws ServletException,IOException  
-{  
-res.setContentType("text/html");//setting the content type  
-PrintWriter pw=res.getWriter();//get the stream to write the data  
-  
-//writing html in the stream  
-pw.println("<html><body>");  
-pw.println("Generador PDF");  
-pw.println("</body></html>");  
-  
-pw.close();//closing the stream  
-}}  
+import javax.servlet.http.*;
+import javax.servlet.*;
+import java.io.*;
+
+import net.sf.jasperreports.engine.*;
+import java.io.*;
+import java.sql.*;
+
+public class GeneradorPdf extends HttpServlet 
+{
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException 
+	{
+		res.setContentType("application/PDF");// setting the content type
+		PrintWriter pw = res.getWriter();// get the stream to write the data
+
+		// writing html in the stream
+		pw.println("<html><body>");
+		pw.println("<embed src=\"http://www.tutorialspoint.com/servlets/pdf/servlets_interview_questions.pdf\" ></embed>");
+		pw.println("</body></html>");
+
+		pw.close();// closing the stream
+	}
+}
